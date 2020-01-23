9701331f115ef46f5e5a5ff311c8413908dfa7df
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-23 21:04:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,13 @@
+
+scale = [2,3,4,5]
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array.length.times do |index|
+break  array.index(value_to_find)
+end
+end
+find_element_index(scale,4)
+
+end
 end
 
 def find_max_value(array)
